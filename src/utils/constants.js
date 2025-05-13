20a65407f68bf563a55377bc8cb82a01a18f5a8b
JoseAValenzuelaB <115498921+JoseAValenzuelaB@users.noreply.github.com>
2025-05-12 23:44:05 -0700

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 // export const API_URL = 'http://142.93.113.77:8000';
-export const API_URL = 'http://127.0.0.1:8000';
-// export const API_URL = 'http://143.198.100.227';
-// export const WS_URL = "ws://143.198.100.227/ws";
-export const WS_URL = "ws://127.0.0.1:8000/ws";
+// export const API_URL = 'http://127.0.0.1:8000';
+export const API_URL = 'http://143.198.100.227';
+export const WS_URL = "ws://143.198.100.227/ws";
+// export const WS_URL = "ws://127.0.0.1:8000/ws";
 export const PAYMENT_TYPE_DICT = {
     "CASH": "Efectivo",
     "CARD": "Tarjeta",
